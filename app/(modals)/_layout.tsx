f8236401baa2c01ea7dc04b16f9aced306bc637f
Elizabeth Martinez <lizmtz713@users.noreply.github.com>
2026-02-18 05:22:43 +0000

    Add settings to modals layout

--- app/(modals)/_layout.tsx
+++ app/(modals)/_layout.tsx
@@ -23,6 +23,7 @@ export default function ModalsLayout() {
       <Stack.Screen name="decode" options={{ presentation: 'modal' }} />
       <Stack.Screen name="relate" options={{ presentation: 'modal' }} />
       <Stack.Screen name="love" options={{ presentation: 'modal' }} />
+      <Stack.Screen name="settings" options={{ presentation: 'modal' }} />
       <Stack.Screen name="identity-setup" options={{ presentation: 'fullScreenModal' }} />
     </Stack>
   );
